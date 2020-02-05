84930379351ac0d5f17ef0a91bcbe9459f8f1503
Johnson Joseph <jhonsonj88@gmail.com>
2020-02-05 23:40:05 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,4 +1,20 @@
-export function manageFriends(state, action){
-
-  
+export function manageFriends(state = {friends: []}, action){
+  // Return state based on action
+  switch (action.type) {
+    // Non-destructively add friend to array
+    case 'ADD_FRIEND':
+      return ({
+        ...state,
+        friends: [...state.friends, action.friend]
+      })
+    // Non-destructively remove friend from array given action index
+    case 'REMOVE_FRIEND':
+      const remIndex = state.friends.findIndex(f => f.id === action.id);
+      return ({
+        ...state,
+        friends: [...state.friends.slice(0, remIndex), ...state.friends.slice(remIndex + 1)]
+        })
+    default:
+      return state;
+  }
 }
