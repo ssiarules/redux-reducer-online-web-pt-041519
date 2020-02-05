5ec445e403f2e60ac06e4bb5f61df80111385767
Johnson Joseph <jhonsonj88@gmail.com>
2020-02-05 23:07:59 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,34 @@
 export function manageFriends(state, action){
+  state = {
+    friends: []
+  }
+
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {friend: {
+
+      }}
+    case "REMOVE_FRIEND":
+      return {}
+
+
+      break;
+    default:
+
+  }
+
 }
+
+/*
+function reducer(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    case 'DECREASE_COUNT':
+      return {count: state.count - 1}
+    default:
+      return state;
+  }
+}
+
+*/
